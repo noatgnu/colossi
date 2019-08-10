162310ce2052687d2b9095cc9e701a290bbca473
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2019-08-10 21:14:09 +1000

    Update settingmain.py

--- settingmain.py
+++ settingmain.py
@@ -2,5 +2,5 @@ import os
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_STATIC = os.path.join(APP_ROOT, "static")
-Rscript = ""
+Rscript = "Rscript"
 R_script_to_be_execute = os.path.join(APP_ROOT, "rScript.R")
